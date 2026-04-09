207be7f18e2cc2f2c29704edc458c3de8836f8ed
Amalsol <amalsoltani340@gmail.com>
2026-04-09 20:57:09 +0200

    Fix final: Chemins des cles SSH

--- variables.tf
+++ variables.tf
@@ -17,6 +17,21 @@ variable "admin_username" {
 variable "vm_size" {
   default = "Standard_B1s"
 }
+
 variable "admin_ip" {
   description = "IP publique de l'administrateur"
+  default     = "0.0.0.0/0"
+}
+
+# AJOUTE CES 3 VARIABLES ICI :
+variable "bastion_public_key_path" {
+  default = "./keys/bastion-vm_key.pem.pub"
+}
+
+variable "web_public_key_path" {
+  default = "./keys/web-vm_key.pem.pub"
+}
+
+variable "db_public_key_path" {
+  default = "./keys/db-vm_key.pem.pub"
 }
