1a58095b78eafd6e8b4ad73ffb309d82dc043efb
Amalsol <amalsoltani340@gmail.com>
2026-05-04 12:32:36 +0200

    update terraform configure (secured)

--- backend.tf
+++ backend.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "rg-pfe-cloud"
     storage_account_name = "tfstatepfe2026"
     container_name       = "tfstate"
     key                  = "pfe.terraform.tfstate"
+    use_azuread_auth     = true
+    # Credentials are now handled securely via shell variables
   }
 }
