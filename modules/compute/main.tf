4fd425f29bdfcbbbb30077a50ae8d1f017dfb117
Amalsol <amalsoltani340@gmail.com>
2026-05-14 11:21:24 +0100

    fix conflict

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -23,12 +23,11 @@ resource "azurerm_network_interface" "bastion_nic" {
 
 # Bastion VM
 resource "azurerm_linux_virtual_machine" "bastion" {
-  name                           = "bastion-vm"
-  location                       = var.location
-  resource_group_name            = var.resource_group_name
-  size                           = var.vm_size
-  admin_username                 = var.admin_username
-  skip_shutdown_and_force_delete = true
+  name                = "bastion-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
 
   network_interface_ids = [azurerm_network_interface.bastion_nic.id]
 
@@ -65,12 +64,11 @@ resource "azurerm_network_interface" "web_nic" {
 
 # Web VM
 resource "azurerm_linux_virtual_machine" "web" {
-  name                           = "web-vm"
-  location                       = var.location
-  resource_group_name            = var.resource_group_name
-  size                           = var.vm_size
-  admin_username                 = var.admin_username
-  skip_shutdown_and_force_delete = true
+  name                = "web-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
 
   network_interface_ids = [azurerm_network_interface.web_nic.id]
 
@@ -101,7 +99,6 @@ resource "azurerm_linux_virtual_machine" "web" {
   EOF
   )
 }
-
 # NIC Database VM
 resource "azurerm_network_interface" "db_nic" {
   name                = "nic-db"
@@ -117,12 +114,11 @@ resource "azurerm_network_interface" "db_nic" {
 
 # Database VM
 resource "azurerm_linux_virtual_machine" "db" {
-  name                           = "db-vm"
-  location                       = var.location
-  resource_group_name            = var.resource_group_name
-  size                           = var.vm_size
-  admin_username                 = var.admin_username
-  skip_shutdown_and_force_delete = true
+  name                = "db-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
 
   network_interface_ids = [azurerm_network_interface.db_nic.id]
 
@@ -143,18 +139,25 @@ resource "azurerm_linux_virtual_machine" "db" {
     version   = "latest"
   }
 
+  # Cloud-init : installer PostgreSQL automatiquement
   custom_data = base64encode(<<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y postgresql postgresql-contrib
     systemctl enable postgresql
     systemctl start postgresql
+    # Créer la base de données PFE
     sudo -u postgres psql -c "CREATE DATABASE pfe_db;"
     sudo -u postgres psql -c "CREATE USER pfe_user WITH PASSWORD 'pfe2026';"
     sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE pfe_db TO pfe_user;"
+    # Permettre les connexions depuis le subnet web
     echo "host pfe_db pfe_user 10.0.2.0/24 md5" >> /etc/postgresql/16/main/pg_hba.conf
     sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '10.0.3.4'/" /etc/postgresql/16/main/postgresql.conf
     systemctl restart postgresql
   EOF
   )
 }
+
+
+
+
