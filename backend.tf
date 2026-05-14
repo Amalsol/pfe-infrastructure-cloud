b1b560736152a37397be073bb4b218d53b4baaa6
Amalsol <amalsoltani340@gmail.com>
2026-05-14 11:34:36 +0100

    fix: bump azurerm provider to 3.90 for skip_shutdown support

--- backend.tf
+++ backend.tf
@@ -3,16 +3,14 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.90"
     }
   }
-
   backend "azurerm" {
     resource_group_name  = "rg-pfe-cloud"
     storage_account_name = "tfstatepfe2026"
     container_name       = "tfstate"
     key                  = "pfe.terraform.tfstate"
     use_azuread_auth     = true
-    # Credentials are now handled securely via shell variables
   }
 }
